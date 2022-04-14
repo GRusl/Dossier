1d954b973db68212f203d57dbe5f49d31e81b4fa
Q <fhsndxva@gmail.com>
2022-04-15 01:55:26 +0300

    It is possible to create publications

--- apps/profile.py
+++ apps/profile.py
@@ -3,6 +3,8 @@ from data import db_session
 from data.user import User
 from data.publication import Publication
 
+from forms.add_publication import AddPublicationForm
+
 from flask import Blueprint, render_template
 
 db_session.global_init('./db/test1.sqlite')
@@ -16,6 +18,28 @@ def profile(pk):
     return render_template('profile/profile.html',
                            title='Посмотреть досье',
                            publications=db_sess.query(Publication).filter(Publication.author == pk))
+
+
+@profile_blueprint.route('/add', methods=['GET', 'POST'])
+def add_publication():
+    form = AddPublicationForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+
+        publication = Publication()
+        publication.author = 1
+        publication.img_id = form.img_id.data
+        publication.title = form.title.data
+        publication.text = form.text.data
+        db_sess.add(publication)
+
+        db_sess.commit()
+
+        return 'ok'
+
+    return render_template('profile/add_publication.html',
+                           title='Посмотреть досье',
+                           form=form)
 
 
 @profile_blueprint.route('/edit')
---
+++ forms/add_publication.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, IntegerField, TextAreaField
+from wtforms.validators import DataRequired
+
+
+class AddPublicationForm(FlaskForm):
+    img_id = IntegerField('id изображения', validators=[DataRequired()])
+
+    title = StringField('Заголовок', validators=[DataRequired()])
+    text = TextAreaField('Содержание', validators=[DataRequired()])
+
+    submit = SubmitField('Сохранить')
