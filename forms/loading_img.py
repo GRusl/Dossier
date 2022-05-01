071923d7daf111edb44a6d8ebdfa8622208c3b87
GRusl <ffhshdxva@gmail.com>
2022-05-01 17:26:40 +0300

    Added a validator to check the uploaded file

--- forms/loading_img.py
+++ forms/loading_img.py
@@ -1,11 +1,18 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed
 
 from wtforms import SubmitField, BooleanField, FileField, TextAreaField
 from wtforms.validators import DataRequired
 
 
 class LoadingImgForm(FlaskForm):
-    file = FileField('Изображение', validators=[DataRequired()])
+    file = FileField(
+        'Изображение',
+        validators=[
+            DataRequired(),
+            FileAllowed(['jpg', 'png'], 'Только .jpg или .png')
+        ]
+    )
 
     description = TextAreaField('Image Description')
 
