f4b8bb367b5801b30c9bd4bac39a157b79f5c05d
GRusl <ffhshdxva@gmail.com>
2022-05-04 23:20:33 +0300

    Commenting app.py

--- app.py
+++ app.py
@@ -12,21 +12,22 @@ from settings import MainDB
 
 from waitress import serve
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'secret_key')
-app.config['DEBUG'] = os.environ.get('DEBUG', False)
-app.config['UPLOAD_FOLDER'] = {'png', 'jpg'}
+app = Flask(__name__)  # Создание приложения
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'secret_key')  # Настройка секретного ключа
+app.config['DEBUG'] = os.environ.get('DEBUG', False)  # Установка режима тестирования
+app.config['UPLOAD_FOLDER'] = {'png', 'jpg'}  # Установка формата загружаемых файлов
 
-login_manager = LoginManager()
-login_manager.init_app(app)
+login_manager = LoginManager()  # Создание объекта регистрации
+login_manager.init_app(app)  # Регистрация объекта регистрации
 
-app.register_blueprint(publication.publication_blueprint, url_prefix='/publication')
-app.register_blueprint(entrance.entrance_blueprint, url_prefix='/entrance')
-app.register_blueprint(profile.profile_blueprint, url_prefix='/profile')
-app.register_blueprint(images.images_blueprint, url_prefix='/images')
+# Регистрация приложений
+app.register_blueprint(publication.publication_blueprint, url_prefix='/publication')  # Работа с публикациями
+app.register_blueprint(entrance.entrance_blueprint, url_prefix='/entrance')  # Обработка входа на страницу
+app.register_blueprint(profile.profile_blueprint, url_prefix='/profile')  # Работа с профилем
+app.register_blueprint(images.images_blueprint, url_prefix='/images')  # Работа с изображениями
 
-db_session.global_init(MainDB.name)
-db_sess = db_session.create_session()
+db_session.global_init(MainDB.name)  # Инициализация БД
+db_sess = db_session.create_session()  # Подключение к БД
 
 
 @login_manager.user_loader
@@ -35,12 +36,12 @@ def load_user(user_id):
 
 
 @app.route('/media/<filename>')
-def media(filename):
+def media(filename):  # Обработка выдачи media
     return send_from_directory('./media/', filename)
 
 
 @app.route('/')
-def index():
+def index():  # Главная страница
     examples = db_sess.query(User).filter(User.example == True)
 
     return render_template('homepage/homepage.html',
@@ -48,5 +49,5 @@ def index():
                            examples=examples)
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # Запуск сервера
     serve(app, host='0.0.0.0', port=5000)
