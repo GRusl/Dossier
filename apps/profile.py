aa022186f9cb476058a366ba79a080b836b72d1d
GRusl <ffhshdxva@gmail.com>
2022-05-04 23:50:16 +0300

    Commenting profile.py

--- apps/profile.py
+++ apps/profile.py
@@ -10,14 +10,14 @@ from data import db_session
 from data.user import User
 from data.publication import Publication
 
-db_session.global_init(MainDB.name)
-db_sess = db_session.create_session()
+db_session.global_init(MainDB.name)  # Инициализация БД
+db_sess = db_session.create_session()  # Подключение к БД
 
-profile_blueprint = Blueprint('profile', __name__)
+profile_blueprint = Blueprint('profile', __name__)  # Создание приложения
 
 
 @profile_blueprint.route('/<int:pk>')
-def profile(pk):
+def profile(pk):  # Отображение профиля
     return render_template('profile/profile.html',
                            title='Посмотреть досье',
                            user=db_sess.query(User).get(pk),
@@ -26,29 +26,31 @@ def profile(pk):
 
 @profile_blueprint.route('/edit', methods=['GET', 'POST'])
 @login_required
-def edit():
-    form = ProfileForm()
-    user = db_sess.query(User).get(current_user.id)
-    if user:
-        if request.method == 'GET':
+def edit():  # Редактирование пользователя
+    form = ProfileForm()  # Инициализация формы
+    user = db_sess.query(User).get(current_user.id)  # Получение обьекта пользователя
+    if user:  # Проверка на существование пользователя
+        if request.method == 'GET':  # Обработка GET запроса
+            # Внесение имеющихся значений в поля
             form.surname.data = user.surname
             form.name.data = user.name
             form.email.data = user.email
             form.age.data = user.age
             form.city.data = user.city
             form.description.data = user.description
-        elif request.method == 'POST' and form.validate_on_submit():
+        elif request.method == 'POST' and form.validate_on_submit():  # Обработка Post запроса
+            # Замена на новые значения
             user.surname = form.surname.data
             user.name = form.name.data
             user.email = form.email.data
             user.age = form.age.data
             user.city = form.city.data
             user.description = form.description.data
-            db_sess.commit()
-            return redirect(url_for('profile.profile', pk=current_user.id))
+            db_sess.commit()  # Применение изменений
+            return redirect(url_for('profile.profile', pk=current_user.id))  # Перенос на просмотр профиля
     else:
-        abort(404)
+        abort(404)  # Ошибка 404
 
-    return render_template('profile/edit.html',
+    return render_template('profile/edit.html',  # Отображение формы
                            title='Редактировать',
                            form=form)
